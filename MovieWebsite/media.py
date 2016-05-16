afa72404f5ff4ee66702bba550dd452f81c29023
orgrune <orgrunebaum@gmail.com>
2016-05-15 21:05:02 -0400

    Updated indent blocks

--- MovieWebsite/media.py
+++ MovieWebsite/media.py
@@ -9,20 +9,20 @@ class Movie():
     
     
     def __init__(self, movie_title, poster_image, trailer_youtube):
-    """ initialize movies to store relevant movie information we need for
-        website.
+        """ initialize movies to store relevant movie information we
+        need for website.
         
         inputs Required: 
         Movie Title : string 
         Movie Poster : URL to a movie poster
-        Movie Trailer : Youtube UR of Trailer
-    """
+        Movie Trailer : Youtube UR of Trailer.
+        """
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-   ers
+   
     def show_trailer(self):
-    """show_trailer() opens the trailer in a new browser window. """
+        """show_trailer() opens the trailer in a new browser window. """
         webbrowser.open(self.trailer_youtube_url)
 
